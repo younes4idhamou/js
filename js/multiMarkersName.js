798541c430e6568fe1da65eeed48bfec8804aa8c
younes4idhamou <yidhamou650@gmail.com>
2022-06-02 13:29:17 +0100

    s

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -31,9 +31,7 @@ AFRAME.registerComponent('markers_start',{
 		  var textEl = document.createElement('a-entity');
 		  
 		  textEl.setAttribute('id','text');
-		  textEl.setAttribute('gltf-model','resources/target/cocaine.pdb');
-		  textEl.object3D.rotation.set(-90, 0, 0);
-		  textEl.object3D.position.set(50,150,0);
+		  textEl.setAttribute('gltf-model','resources/target/'+(k+1)+'.gltf');
 
 		  markerEl.appendChild(textEl);
 		  
